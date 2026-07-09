6fe8a396c13363699dbdf8e4df31d1e8f979a291
Cleriston Martinelo <cleriston@gmail.com>
2026-07-08 20:44:20 -0400

    New version of mlflow

--- backend/config.py
+++ backend/config.py
@@ -2,6 +2,14 @@ from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent
 
-MODEL_PATH = BASE_DIR / "models" / "best.pt"
+MLFLOW_USER = "mlflowsuperuser"
+MLFLOW_PASS = "wfu:1H6B_pFLe',2\""
+MLFLOW_HOST = "cleristonm.duckdns.org"
+
+TRACKING_URI = (
+    f"http://{MLFLOW_USER}:{MLFLOW_PASS}@{MLFLOW_HOST}:5000"
+)
+
+EXPERIMENT_NAME = "foodseg-yolo"
 
 NUTRITION_PATH = BASE_DIR / "assets" / "nutrition.json"
--- backend/services/detection.py
+++ backend/services/detection.py
@@ -1,7 +1,53 @@
+import mlflow
+from mlflow import MlflowClient
 from ultralytics import YOLO
-from config import MODEL_PATH
 
-model = YOLO(MODEL_PATH)
+from config import TRACKING_URI, EXPERIMENT_NAME
+
+mlflow.set_tracking_uri(TRACKING_URI)
+
+client = MlflowClient()
+
+
+def load_best_model():
+
+    experiment = client.get_experiment_by_name(EXPERIMENT_NAME)
+
+    if experiment is None:
+        raise Exception(f"Expérience '{EXPERIMENT_NAME}' introuvable.")
+
+    runs = client.search_runs(
+        experiment_ids=[experiment.experiment_id],
+        order_by=["metrics.map50_mask DESC"],
+        max_results=1,
+    )
+
+    if not runs:
+        raise Exception("Aucun modèle trouvé.")
+
+    best_run = runs[0]
+
+    run_id = best_run.info.run_id
+
+    print(f"Best Run : {run_id}")
+    print(
+        f"mAP50 Mask : "
+        f"{best_run.data.metrics.get('map50_mask')}"
+    )
+
+    artifact_uri = (
+        f"runs:/{run_id}/training/weights/best.pt"
+    )
+
+    local_model = mlflow.artifacts.download_artifacts(
+        artifact_uri=artifact_uri
+    )
+
+    return YOLO(local_model)
+
+
+model = load_best_model()
+
 
 def detect(image_path):
 
